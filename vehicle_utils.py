136927d5a82a881dc45ccd030ec931bced286d08
Claudia Sychev <csychev@stedwards.edu>
2020-04-28 22:03:37 +0000

    vehicle_utils.py comment edits

--- vehicle_utils.py
+++ vehicle_utils.py
@@ -16,11 +16,11 @@ def getRoute(begin_long,begin_lat,end_long, end_lat ):
         return coords
 
 def getHumanReadable(begin_long,begin_lat):
-        #Make request for mapbox driving data given two coordinates
+        #Make request for mapbox driving data given a set of coordinates
         mapbox_driving_req = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/'+str(begin_long)+','+str(begin_lat)+'.json?access_token=pk.eyJ1IjoiY3N5Y2hldiIsImEiOiJjazZsbmg4c2gwYXU3M21zOG55aTljcTBuIn0.G5UXjF-3_0mXKo6huFgLwg&place_name')
         #encode data
         data = mapbox_driving_req.json()
-        #json dump coordinates data
+        #json dump place name data
         address = json.dumps(data.get("features")[0].get('place_name'))
         return address
 
